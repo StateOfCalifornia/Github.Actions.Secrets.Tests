76329c8813e6291acf71889a1c1dd42517058e4e
toiyabe <52581997+toiyabe@users.noreply.github.com>
2021-09-02 13:16:25 -0700

    Add project files.

---
+++ Github.Actions.Secrets.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Github.Actions.Secrets.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var data = Environment.GetEnvironmentVariable("HelloWorld");
+            throw new Exception("HelloWorld = [" + data + "]");
+        }
+    }
+}
