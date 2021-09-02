c2fa473d94be67b19253a04c58a576cf069000a0
toiyabe <52581997+toiyabe@users.noreply.github.com>
2021-09-02 14:49:53 -0700

    all caps env var

--- Github.Actions.Secrets.Tests/UnitTest1.cs
+++ Github.Actions.Secrets.Tests/UnitTest1.cs
@@ -6,10 +6,21 @@ namespace Github.Actions.Secrets.Tests
     [TestClass]
     public class UnitTest1
     {
+
+        public TestContext TestContext { get; set; }
+
+        private static TestContext _testContext;
+
+        [ClassInitialize]
+        public static void SetupTests(TestContext testContext)
+        {
+            _testContext = testContext;
+        }
+
         [TestMethod]
         public void TestMethod1()
         {
-            var data = Environment.GetEnvironmentVariable("EnvHelloWorld");
+            var data = Environment.GetEnvironmentVariable("ENVHELLOWORLD");
             throw new Exception("HelloWorld = [" + data + "]");
         }
     }
